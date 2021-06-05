5df2f29f5382f206b18ef939bd023efded3bbd4b
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-05 23:51:20 +0530

    Add batch commit method

--- committer.sh
+++ committer.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+batch_commit() {
+    echo -e "\u001b[34;1mBatch Comitting: $(pwd | awk -F "/" '{print $NF}') \u001b[0m"
+    for file in $(ls "$flags"); do
+        git add "$file"
+        echo -e "\u001b[32;1mAdded $file \u001b[0m"
+        git commit -m "Add $file"
+    done
+}
+
 select_files() {
     more="Y"
     while [[ "$more" == "Y" || "$more" == "y" ]]; do
         PS3=$(echo -en "\u001b[34;1m=> Enter file number: \u001b[0m")
         echo -en "\u001b[29;1m"
-        select file in $(ls -A$t); do
+        select file in $(ls "$flags"); do
             echo -en "\u001b[31;1mAdd $file? (y/n) \u001b[0m"
             read -r input
             if [[ "$input" == "Y" || "$input" == "y" ]]; then
