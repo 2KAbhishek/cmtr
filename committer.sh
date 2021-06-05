e22a4b6da260b1b2bf79e18cdaf8cd39c4582a37
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-05 20:52:12 +0530

    Show hidden files

--- committer.sh
+++ committer.sh
@@ -2,14 +2,14 @@
 
 if  [[ $1 == "-a" ]];
 then
-    for file in $(ls -atr); do
+    for file in $(ls -Atr); do
     git add "$file"
     echo -e "\u001b[32;1m Added $file \u001b[0m"
     git commit -m "Add $file"
     done
 else
 PS3="Enter file number: "
-select file in $(ls -at); do
+select file in $(ls -At); do
 
     echo -en "\u001b[34;1m Add $file? (y/n) \u001b[0m"
     read -r input
