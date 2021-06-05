2858773063cd56a543c334ce4753d6c045e0f574
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-05 20:51:05 +0530

    Add committer.sh

--- committer.sh
+++ committer.sh
@@ -2,14 +2,14 @@
 
 if  [[ $1 == "-a" ]];
 then
-    for file in $(ls -tr); do
+    for file in $(ls -atr); do
     git add "$file"
     echo -e "\u001b[32;1m Added $file \u001b[0m"
     git commit -m "Add $file"
     done
 else
 PS3="Enter file number: "
-select file in $(ls -t); do
+select file in $(ls -at); do
 
     echo -en "\u001b[34;1m Add $file? (y/n) \u001b[0m"
     read -r input
