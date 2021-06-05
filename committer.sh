e644e7914faa641573c2c56e586fa97dfade284a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-05 23:42:31 +0530

    Extract select files method

--- committer.sh
+++ committer.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-if [[ $1 == "-a" ]]; then
-    echo -e "\u001b[34;1mAuto Comitting: $(pwd | awk -F "/" '{print $NF}') \u001b[0m"
-    for file in $(ls -Atr); do
-        git add "$file"
-        echo -e "\u001b[32;1mAdded $file \u001b[0m"
-        git commit -m "Add $file"
-    done
-else
+
+select_files() {
     more="Y"
     while [[ "$more" == "Y" || "$more" == "y" ]]; do
         PS3=$(echo -en "\u001b[34;1m=> Enter file number: \u001b[0m")
         echo -en "\u001b[29;1m"
-        select file in $(ls -At); do
+        select file in $(ls -A$t); do
             echo -en "\u001b[31;1mAdd $file? (y/n) \u001b[0m"
             read -r input
             if [[ "$input" == "Y" || "$input" == "y" ]]; then
