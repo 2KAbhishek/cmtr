564ad5f2b8d629381390f6215c1d44c189794a42
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-05 22:57:36 +0530

    Major changes, related to UX

--- committer.sh
+++ committer.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-
-if  [[ $1 == "-a" ]];
-then
+if [[ $1 == "-a" ]]; then
+    echo -e "\u001b[34;1mAuto Comitting: $(pwd | awk -F "/" '{print $NF}') \u001b[0m"
     for file in $(ls -Atr); do
-    git add "$file"
-    echo -e "\u001b[32;1m Added $file \u001b[0m"
-    git commit -m "Add $file"
+        git add "$file"
+        echo -e "\u001b[32;1mAdded $file \u001b[0m"
+        git commit -m "Add $file"
     done
 else
-PS3="Enter file number: "
-select file in $(ls -At); do
+    more="Y"
+    while [[ "$more" == "Y" || "$more" == "y" ]]; do
+        PS3=$(echo -en "\u001b[34;1m=> Enter file number: \u001b[0m")
+        echo -en "\u001b[29;1m"
+        select file in $(ls -At); do
+            echo -en "\u001b[31;1mAdd $file? (y/n) \u001b[0m"
+            read -r input
+            if [[ "$input" == "Y" || "$input" == "y" ]]; then
+                git add "$file"
+                echo -e "\u001b[32;1mAdded $file \u001b[0m"
+            else
+                break
+            fi
 
-    echo -en "\u001b[34;1m Add $file? (y/n) \u001b[0m"
-    read -r input
-    if [[ "$input" == "Y" || "$input" == "y" ]];
-    then
-        git add "$file"
-        echo -e "\u001b[32;1m Added $file \u001b[0m"
-    else
-        continue
-    fi
-
-    echo -en "\u001b[34;1m Enter commit message: \u001b[0m"
-    read -r msg
-    if [ -z "$msg" ]
-    then
-        git commit -m "Add $file"
-        continue
-    else
-        git commit -m "$msg"
-        continue
-    fi
-
+            echo -en "\u001b[32;1m=> Enter commit message: \u001b[0m"
+            read -r msg
+            if [ -z "$msg" ]; then
+                git commit -m "Add $file"
+                break
+            else
+                git commit -m "$msg"
+                break
+            fi
+        done
+            echo -en "\u001b[36;1mAdd more? (y/n) \u001b[0m"
+            read -r more
     done
 fi
